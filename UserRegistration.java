81cdcf2c55150a45832b5b66ead277023d828463
ganeshalapati <alapatiganesh31@gmail.com>
2021-08-05 21:57:47 +0530

    Added a method to validate LastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -22,17 +22,38 @@ public class UserRegistration {
 		     else {
 		    	 System.out.println("Invalid username");
 		     } }
+		 
+		 //method to check LastName Valid or Invalid	
+		 public void validateLastname() {
+			 System.out.println("Enter LastName");
+			 String lastName=sc.next();
+			 //regex pattern for Lastname
+			 String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
+			 Pattern p = Pattern.compile(regex);
+			 Matcher matcher = p.matcher(lastName);
+			 boolean result = matcher.matches();
+			 
+		     if(result){
+		    	 System.out.println("Valid username");
+		     }
+		     else {
+		    	 System.out.println("Invalid username");
+		     } 
+		}
 		
 		 public static void main(String[] args) {
 				UserRegistration registration=new UserRegistration();
 				Scanner sc=new Scanner(System.in);
 				while(true) {
 					//userinput choice for checking Multiple validation
-					System.out.println("Enter choce\n 1)UserName\n");
+					System.out.println("Enter choce\n 1)UserName\n 2)LastName\n");
 					int choice=sc.nextInt();
 					switch(choice) {
 					case 1:
 						registration.validateUsername();
+						break;
+					case 2:
+						registration.validateLastname();
 						break;
 					default:
 						System.out.println("Invalid choice");
